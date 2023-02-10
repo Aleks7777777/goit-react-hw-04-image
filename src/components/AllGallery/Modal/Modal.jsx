d71202114fd695a815670055a72cb236e57e285a
Aleks <777777ot@gmail.com>
2023-02-10 22:27:53 +0200

    777

---
+++ src/components/AllGallery/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import { Overlay, OverlayContent } from './Modal.styled';
+// import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+class Modal extends Component {
+	componentDidMount() {
+		window.addEventListener('keydown', this.handleKeyEscape);
+	}
+	componentWillUnmount() {
+		window.removeEventListener('keydown', this.handleKeyEscape);
+	}
+	handleKeyEscape = e => {
+		if (e.code === 'Escape') {
+			this.props.onClose();
+		}
+	};
+	handleBackdropClick = e => {
+		if (e.target === e.currentTarget) {
+			this.props.onClose();
+		}
+	};
+
+	render() {
+		return (
+			<Overlay className="overlay" onClick={this.handleBackdropClick}>
+				<OverlayContent className="modal">
+					<img src={this.props.modalImg} alt="12" />
+				</OverlayContent>
+			</Overlay>
+		);
+	}
+}
+
+// Modal.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+//   modalImg: PropTypes.string.isRequired,
+// };
+export default Modal;
